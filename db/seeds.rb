f73c450a509bd1c4ab347dca9eda55919d6b1eac
laurenmetz17 <103585965+laurenmetz17@users.noreply.github.com>
2023-04-03 15:08:35 -0400

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,8 @@
 puts "🌱 Seeding spices..."
-
 # Seed your database here
+ShoeStore.create(name:"Dr.Martens", address: "Kenwood Mall", total_inventory: 0)
+Shoe.create(name:"1460", style: "boot", price: 170, color: "black", shoe_store_id:1)
+Shoe.create(name:"Jadon", style: "boot", price: 200, color: "black", shoe_store_id:1)
+Shoe.create(name:"1461", style: "loafer", price: 130, color: "beige", shoe_store_id:1)
 
 puts "✅ Done seeding!"
