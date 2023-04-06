60a97fa8157ed52866f7c451bcb1945964041841
laurenmetz17 <103585965+laurenmetz17@users.noreply.github.com>
2023-04-06 12:13:22 -0400

    add methods to shoe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
+
+
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
   get "/" do
     { message: "Good luck with your project!" }.to_json
+  end
+
+  get "/inventory" do
+    inventory = Shoe.all
+    inventory.to_json
   end
 
 end
--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -1,3 +1,20 @@
 class Shoe < ActiveRecord::Base
     belongs_to :shoe_store
+
+    def self.by_price
+        self.all.order(price: :desc)
+    end
+
+    def self.by_style(style)
+        self.all.find_by(style: style)
+    end
+
+    def self.by_store(store)
+        self.all.find_by(store_id: store)
+    end
+
+    def self.by_color(color)
+        self.all.find_by(color: color)
+    end
+
 end
