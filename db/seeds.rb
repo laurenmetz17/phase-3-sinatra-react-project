9c4f4041b768792f13478bc54bf5665ad9ac0ebc
laurenmetz17 <103585965+laurenmetz17@users.noreply.github.com>
2023-04-07 19:54:10 -0400

    update seed to find store by name

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 puts "🌱 Seeding spices..."
 # Seed your database here
 ShoeStore.create(name:"Dr.Martens", address: "Kenwood Mall", total_inventory: 0)
-Shoe.create(name:"1460", style: "boot", price: 170, color: "black", shoe_store_id:1)
-Shoe.create(name:"Jadon", style: "boot", price: 200, color: "black", shoe_store_id:1)
-Shoe.create(name:"1461", style: "loafer", price: 130, color: "beige", shoe_store_id:1)
+Shoe.create(name:"1460", style: "boot", price: 170, color: "black", shoe_store_id:ShoeStore.id_by_name("Dr.Martens"))
+Shoe.create(name:"Jadon", style: "boot", price: 200, color: "black", shoe_store_id:ShoeStore.id_by_name("Dr.Martens"))
+Shoe.create(name:"1461", style: "loafer", price: 130, color: "beige", shoe_store_id:ShoeStore.id_by_name("Dr.Martens"))
 
 puts "✅ Done seeding!"
