9f85fc1c570951b9d6392fdf90fe628a012bc128
laurenmetz17 <103585965+laurenmetz17@users.noreply.github.com>
2023-04-07 19:51:06 -0400

    add id by name for store

--- app/models/shoe_store.rb
+++ app/models/shoe_store.rb
@@ -4,4 +4,9 @@ class ShoeStore < ActiveRecord::Base
     def self.by_name(name)
         self.all.find_by(name: name)
     end
+
+    def self.id_by_name(name)
+        store = self.by_name(name)
+        store.id
+    end
 end
